5ea8d721a4437b2571dc716c48adedeca157d60d
Federico-creator <fbornico@udesa.edu.ar>
2021-04-11 12:09:42 -0300

    productos

--- proyectoIntegrador/controllers/productosControllers.js
+++ proyectoIntegrador/controllers/productosControllers.js
@@ -1,10 +1,17 @@
+let autos= require("../data/autos")
 let productosControllers ={
         index: (req, res) => {
             res.send(`Lista de productos`);
         },
         busqueda: (req, res) => {
-            productos=req.params.productos
-            res.send(`Lista de productos por nombre: ${productos}`);
+            let productos=req.params.productos
+            let resultados= []
+            for(let i=0;i<autos.lista.length;i+=1){
+                if(autos.lista[i].modelo==productos){
+                    resultados.push(autos.lista[i])
+                }
+            }
+            res.render("products.ejs", {"products": resultados})
         }
         
     }
---
+++ proyectoIntegrador/data/autos.js
@@ -0,0 +1,79 @@
+const autos={
+    lista: [
+        {
+            marca: "Chevrolet",
+            modelo: "Cruze",
+            año: "2020",
+            color: "Marron",
+            foto: "/images/products/chevrolet-cruze.jpeg",
+            descripcion: "Chevrolet Cruze II 1.4 Sedan At Ltz, (2020)",
+            comentarios: 7
+        },
+        {
+            marca: "Toyota",
+            modelo: "Corolla",
+            año: "2021",
+            color: "Blanco",
+            foto: "/images/products/toyota-corolla.jpeg",
+            descripcion: "Toyota Corolla 1.8 Xei Cvt 140cv, (2021)",
+            comentarios: 20      
+        },
+        {
+            marca: "Honda",
+            modelo: "Civic",
+            año: "2018",
+            color: "Gris",  
+            foto: "/images/products/honda-civic.jpeg",
+            descripcion: "Honda Civic 2.0 Ex-l, (2018)",
+            comentarios: 10
+        },
+        {
+            marca: "Volkswagen",
+            modelo: "T-Cross",
+            año: "2021",
+            color: "Marron",  
+            foto: "/images/products/volkswagen-T-Cross.jpeg",
+            descripcion: "Volkswagen T-Cross 1.6 Comfortline At, (2021)",
+            comentarios: 9
+        },
+        {
+            marca: "Ford",
+            modelo: "F-150",
+            año: "2020",
+            color: "Azul",  
+            foto: "/images/products/ford-f-150.jpeg",
+            descripcion: "Ford F-150 Raptor 3.5l Bi-turbo, (2020)",
+            comentarios: 62
+        },
+        {
+            marca: "Jeep",
+            modelo: "Compass",
+            año: "2020",
+            color: "Negro",  
+            foto: "/images/products/jeep-compass.jpeg",
+            descripcion: "Jeep Compass 2.0 Td At9 4x4 Limited Plus, (2020)",
+            comentarios: 47
+        },
+        {
+            marca: "Volkswagen",
+            modelo: "Vento",
+            año: "2021",
+            color: "Gris",  
+            foto: "/images/products/vento.jpeg",
+            descripcion: "Volkswagen Vento 1.4 Highline 150cv At, (2021)",
+            comentarios: 35
+        },
+        {
+            marca: "Peugeot",
+            modelo: "3008",
+            año: "2020",
+            color: "Blanco",  
+            foto: "/images/products/peugeot-3008.jpeg",
+            descripcion: "Peugeot 3008 1.6 Gt-line Thp Tiptronic, (2020)",
+            comentarios: 27
+        },
+
+
+    ]
+}
+module.exports=autos
