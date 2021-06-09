a33988ac5eae7e0b8332d1f4959887661730215a
Laura Rolon <mrolonmartinez@udesa.edu.ar>
2021-06-08 22:35:38 -0300

    Comentarios funcionando

--- proyectoIntegrador/controllers/productosControllers.js
+++ proyectoIntegrador/controllers/productosControllers.js
@@ -1,50 +1,67 @@
 const db = require("../database/models")
 const op = db.Sequelize.Op;
-let productosControllers ={
-        index: (req, res) => {
-            res.render(`secreto`);
-        },
-        busqueda: (req, res) => {
-            db.Producto.findByPk(`${req.params.id}`)
-                .then(products=>{ 
-                    db.Comentario.findAll({
-                        where: {
-                            
-                            productos_id: products.id
-                        },
-                        include: [
-                            {association: "usuarios"},
-                            ] 
-                            
-                    })
-                    .then(comentarios=>{
-                        return res.render("products", {products, comentarios});
+let productosControllers = {
+    index: (req, res) => {
+        res.render(`secreto`);
+    },
+    busqueda: (req, res) => {
+        db.Producto.findByPk(`${req.params.id}`)
+            .then(products => {
+                db.Comentario.findAll({
+                    where: {
+
+                        productos_id: products.id
+                    },
+                    include: [
+                        { association: "usuarios" },
+                    ]
+
+                })
+                    .then(comentarios => {
+                        return res.render("products", { products, comentarios });
                     }
                     )
-        
-                })
 
-                .catch(err=> console.log(err))
-        },
-        logueado: (req, res) => {
-            let logueado = "logueado"
-            db.Producto.findByPk(`${req.params.id}`)
-                .then(products=>{ 
-                    return res.render("products", {products,logueado});
-                })
-                .catch(err=> console.log(err))
-        },
-        borrar: (req, res)=>{
-            let primaryKey = req.params.id;
-            db.Producto.destroy({
-                where: {
-                    id: primaryKey
-                }
             })
-            .then(()=> res.redirect('/'))
-            .catch(err=> console.log(err))
-        },
-        
-    }
+
+            .catch(err => console.log(err))
+    },
+    comentar: (req, res) => {
+        const comentario = {
+            created_at: new Date(),
+            updated_at: new Date(),
+            texto: req.body.comentario,
+            productos_id: req.params.id,
+            usuarios_id: req.session.user.id
+        }
+        db.Comentario.create(comentario)
+            .then(() =>
+                res.redirect(`/productos/busqueda/${req.params.id}`)
+            )
+            .catch(err => console.log(`el error es ${err}`))
+
+
+
+    },
+    logueado: (req, res) => {
+        let logueado = "logueado"
+        db.Producto.findByPk(`${req.params.id}`)
+            .then(products => {
+                return res.render("products", { products, logueado });
+            })
+            .catch(err => console.log(err))
+    },
+    borrar: (req, res) => {
+        let primaryKey = req.params.id;
+        db.Producto.destroy({
+            where: {
+                id: primaryKey
+            }
+        })
+            .then(() => res.redirect('/'))
+            .catch(err => console.log(err))
+    },
+
+}
 
 module.exports = productosControllers;
--- proyectoIntegrador/routes/productos.js
+++ proyectoIntegrador/routes/productos.js
@@ -13,6 +13,7 @@ router.post("/agregar",agregarControllers.agregar);
 router.get("/borrar/:id",productosControllers.borrar);
 router.get("/editar/:id",editarControllers.index);
 router.post("/editar/:id",editarControllers.post);
+router.post('/busqueda/:id/comentario', productosControllers.comentar);
 
 
 module.exports = router;
