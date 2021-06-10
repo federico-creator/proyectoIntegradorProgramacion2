4750acd1c340321ecf233057abeaafdf538e54a7
mariapaulaherrera <mpherrera@udesa.edu.ar>
2021-06-10 12:31:56 -0300

    pagina usuarios (falta)

--- proyectoIntegrador/app.js
+++ proyectoIntegrador/app.js
@@ -12,6 +12,7 @@ var edicionRouter = require('./routes/edicion');
 var comentariosRouter = require('./routes/comentarios');
 var productosRouter = require('./routes/productos');
 var busqedaRouter = require('./routes/busqueda');
+var usuariosRouter = require('./routes/usuarios');
 var session = require('express-session');
 const db = require('./database/models')
 
@@ -64,6 +65,7 @@ app.use('/edicion', edicionRouter);
 app.use('/comentarios', comentariosRouter);
 app.use('/productos', productosRouter);
 app.use('/busqueda', busqedaRouter);
+app.use('/usuarios', usuariosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
