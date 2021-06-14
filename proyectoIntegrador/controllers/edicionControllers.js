23ada73c2b21d0b990a2fa9d28bbef65a06f3ed1
Federico-creator <fbornico@udesa.edu.ar>
2021-06-14 12:28:45 -0300

    perfilcompleto

--- proyectoIntegrador/controllers/edicionControllers.js
+++ proyectoIntegrador/controllers/edicionControllers.js
@@ -1,5 +1,6 @@
 const db = require("../database/models")
 const op = db.Sequelize.Op;
+const bcrypt = require('bcryptjs');
 let edicionControllers = {
         index:(req, res) =>{    
                 db.Usuario.findByPk(`${req.session.user.id}`)
@@ -8,8 +9,15 @@ let edicionControllers = {
             
         },
         edicion:(req,res)=>{
+                console.log(req.body);
                 let primaryKey = req.session.user.id; 
-                let actualizarusuario = req.body
+                let actualizarusuario =  {nombre: req.body.nombre,
+                apellido: req.body.apellido,
+                documento: req.body.documento,
+                fecha_de_nacimiento: req.body.fecha_de_nacimiento,
+                mail: req.body.mail,
+                password: bcrypt.hashSync(req.body.password, 10),
+                avatar: `/images/users/${req.file.filename}`}
                 db.Usuario.update(actualizarusuario,{where:{id:primaryKey}})
                     .then(()=> res.redirect("/"))
                     .catch(err => console.log(err))
--- proyectoIntegrador/routes/edicion.js
+++ proyectoIntegrador/routes/edicion.js
@@ -1,8 +1,19 @@
 let express = require('express');
 let router = express.Router();
+const multer = require("multer")
+const path = require("path")
 const edicionControllers = require('../controllers/edicionControllers');
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+          cb(null, 'public/images/users')
+    },
+    filename: (req, file, cb) => {
+          cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+  })
+var upload = multer({ storage: storage });
 
 /* GET registration page. */
 router.get('/', edicionControllers.index);
-router.post('/', edicionControllers.edicion);
+router.post('/', upload.single("avatar"),edicionControllers.edicion);
 module.exports = router;
